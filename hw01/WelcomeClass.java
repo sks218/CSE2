d2b242bfb4ae9bd3d6f505178fed6960315fd782
sks218 <sks218@lehigh.edu>
2015-01-26 21:08:06 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+/* 
+Sahib Singh (sks218)
+1/27/15
+hw01
+WelcomeClass
+*/
+
+public class WelcomeClass{
+    
+    public static void main(String[] args){
+        
+        System.out.println("   -----------  "); //these are all print statements.
+        System.out.println("   | WELCOME |  "); //println prints a new line
+        System.out.println("   -----------  ");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-S--K--S--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("");
+        System.out.println("I went on a cruise this past winter break to the Bahamian islands. I went jetskiing and had a blast with my friends!");
+        System.out.println("I went jetskiing and had a blast with my friends!");
+        
+    }
+}
---
+++ lab01-Hello World/HelloWorld.java
@@ -0,0 +1,19 @@
+/*Sahib Singh
+HelloWorld
+1/23/15
+*/
+
+public class HelloWorld{
+    public static void main(String[] args){
+        
+        System.out.println("#########################");  //println prints a new line after the text
+        System.out.println("#########################");
+        System.out.println("##                     ##");
+        System.out.println("##  Hello World 2015!  ##");
+        System.out.println("##                     ##");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        
+        
+    }
+}
