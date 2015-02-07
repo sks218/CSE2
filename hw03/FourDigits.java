b98d87996563069a0c821b782e444c8be8b91ed7
sks218 <sks218@lehigh.edu>
2015-02-07 20:43:57 +0000

    updated hw03

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -14,6 +14,6 @@ public class FourDigits{
         double num = myScanner.nextDouble();
         double num_1 = num - (int) num; //removes numbers to the left of the decimal point
         double num_2 = num_1*10000;
-        System.out.println(df.format(num_2));
+        System.out.println("The four digits to the right of the double: " +df.format(num_2));
     }
 }
