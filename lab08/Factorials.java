7e1b23ccdb7446c3ce06a2f009c48e1e72630513
sks218 <sks218@lehigh.edu>
2015-03-20 18:21:30 +0000

    updated lab08

---
+++ lab08/Factorials.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Factorials{
+    
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        boolean check = true;
+        int x = 0;
+        int sum = 0;
+        while(check){ //continues to prompt user if there is invalid input
+            System.out.println("Enter an integer: ");
+            if(myScanner.hasNextInt()){
+                x = myScanner.nextInt();
+                check = false;
+            }
+            else{
+                System.out.println("Invalid Input!");
+            }
+        }
+        for(int i=x; i>0; i--){
+
+            sum += factorial(i);
+        }
+        System.out.println("");
+        print(sum);
+    }
+    
+    public static void print(int num){ //method that prints factorial once computed
+        System.out.println("The super factorial of the input is: " + num);
+    }
+    
+    public static int factorial(int input){ //method calculates factorial of num;
+        int num = 1;
+        for(int i= input; i>0; i--){
+            num *=i;
+        }
+        System.out.print(num + " ");
+        return num;
+    }
+    
+}
